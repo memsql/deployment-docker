b105a9a1d559fd9233a8f95d453202633b5b6dcf
Wei Zheng <wzheng@memsql.com>
2020-07-27 11:37:01 -0700

    [operator:feature] Enable incremental backup
    Summary:
    Support incremental backup
    Update Dockerfile-tools to include python script for cluster backup, and will use memsql/tools
    image for backup cronjob
    
    Test Plan:
    See the details in https://memsql.atlassian.net/browse/MCDB-956. And in addition to that,
    will add integration test in helios repo.
    
    Reviewers: jkoh, tgummalla, fmagalhaes
    
    Reviewed By: jkoh, tgummalla
    
    Subscribers: eli, cparker, engineering-list
    
    JIRA Issues: MCDB-956
    
    Differential Revision: https://grizzly.internal.memcompute.com/D40554

--- assets/backup/memsql_backup.py
+++ assets/backup/memsql_backup.py
@@ -17,6 +17,81 @@ def GetenvOrFail(var, redact_log=False):
     logging.info("Got {} = {}".format(var, val if not redact_log else "<redacted>"))
     return val
 
+class BackupInfo:
+    def __init__(self, path, is_incremental):
+        self.path = path
+        self.is_incremental = is_incremental
+
+def GenerateInitBackupPath(backup_bucket, backup_prefix):
+    timestamp = time.strftime("%Y-%m-%d-%H-%M", time.gmtime())
+    return "{}/{}_{}".format(backup_bucket, backup_prefix, timestamp)
+
+def FullPathForBackup(cursor, db, is_incremental, full_backup_only, backup_bucket, backup_prefix):
+    if full_backup_only:
+        backup_location = GenerateInitBackupPath(backup_bucket, backup_prefix)
+        return BackupInfo(backup_location, False)
+
+    query_options = {}
+    query_options["db"] = db
+    query_options["bucket"] = backup_bucket
+    query_options["path_prefix"] = backup_prefix
+
+    # When it is a incremental backup job but there is no existing init backup or there is no init backup
+    # job in progress, the incremental backup job will try to do a init backup.
+
+    # If this is an incremental backup job, but there is a init backup for the same db that is in progress,
+    # skip this job.
+    # If this is an a init backup job, but there is a init backup in progress (could be a incremental 
+    # backup job doing a init backup), skip this init backup job.
+    query_template=(
+        "SELECT "
+        "command , state, info "
+        "FROM information_schema.processlist "
+        "WHERE command = 'Query' AND "
+        "info LIKE 'BACKUP DATABASE {db} "
+        "WITH INIT to S3 \"{bucket}/{path_prefix}\_%'")
+    expanded_query = query_template.format(**query_options)
+    logging.info(expanded_query)
+    cursor.execute(expanded_query)
+    rows = cursor.fetchall()
+    if rows:
+        logging.info("INIT backup in progress for DB {} at {}/{}_..., skipping this {} backup"
+                .format(db, query_options["bucket"], query_options["path_prefix"],
+                    "incremental" if is_incremental else "init"))
+        return BackupInfo("", is_incremental)
+
+    if not is_incremental:
+        backup_location = GenerateInitBackupPath(backup_bucket, backup_prefix)
+        return BackupInfo(backup_location, False)
+
+    # there is no init backup in progress, check to see if there are existing init backups.
+    query_template = (
+        "SELECT "
+        "backup_id, incr_backup_id, backup_path, status "
+        "FROM information_schema.mv_backup_history "
+        "WHERE "
+        "database_name = '{db}' AND "
+        "incr_backup_id IS NOT NULL AND "
+        "status = 'Success' AND "
+        "backup_path LIKE '{bucket}/{path_prefix}\_%' "
+        "ORDER BY backup_id DESC, incr_backup_id DESC "
+        "LIMIT 1")
+
+    expanded_query = query_template.format(**query_options)
+    logging.info(expanded_query)
+    cursor.execute(expanded_query)
+    rows = cursor.fetchall()
+
+    if rows:
+        # do an incremental backup based on the latest init backup
+        backup_path = rows[0]['backup_path'] if type(rows[0]) is dict else rows[0][2]
+        return BackupInfo(backup_path[0:backup_path.rindex("/")], True)
+    else:
+        logging.info("There is no INIT backups for DB {} at {}/{}_..., doing INIT backup instead"
+                .format(db, query_options["bucket"], query_options["path_prefix"]))
+        backup_location = GenerateInitBackupPath(backup_bucket, backup_prefix)
+        return BackupInfo(backup_location, False)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)-15s: %(levelname)-8s: %(message)s")
@@ -25,6 +100,9 @@ if __name__ == "__main__":
     backup_creds = {}
     backup_creds["aws_access_key_id"] = GetenvOrFail("AWS_ACCESS_KEY")
     backup_creds["aws_secret_access_key"] = GetenvOrFail("AWS_SECRET_ACCESS_KEY", True)
+    is_incremental = GetenvOrFail("MEMSQL_BACKUP_INCREMENTAL") == "true"
+    full_backup_only_env = getenv("MEMSQL_FULL_BACKUP_ONLY")
+    full_backup_only = full_backup_only_env and full_backup_only_env == "true" and not is_incremental
 
     # Get the BACKUP CONFIG paramaters from the environment
     backup_config = {}
@@ -56,14 +134,11 @@ if __name__ == "__main__":
 
     memsql_password = GetenvOrFail("MEMSQL_PASSWORD", True)
 
-    # Name the backup
-    backup_prefix = GetenvOrFail("MEMSQL_BACKUP_NAME")
-    timestamp = time.strftime("%Y-%m-%d-%H-%M", time.gmtime())
-    backup_base_name = "{}_{}".format(backup_prefix, timestamp)
-
     backup_bucket = GetenvOrFail("MEMSQL_BACKUP_BUCKET")
+    backup_prefix = GetenvOrFail("MEMSQL_BACKUP_NAME")
 
     logging.info("Connecting to target cluster")
+
 
     # Connect to the target cluster
     db = MySQLdb.connect(host=memsql_hostname, user=memsql_user, passwd=memsql_password)
@@ -78,16 +153,25 @@ if __name__ == "__main__":
     logging.info("Found {} databases".format(len(dbs)))
 
     # Run the backups
-    query_template = "BACKUP DATABASE `{db}` TO S3 \"{bucket}/{path}\" CONFIG '{config}' CREDENTIALS '{credentials}'"
+    query_template = "BACKUP DATABASE `{db}` WITH {mode} TO S3 \"{path}\" CONFIG '{config}' CREDENTIALS '{credentials}'"
+    if full_backup_only:
+        query_template = "BACKUP DATABASE `{db}` TO S3 \"{path}\" CONFIG '{config}' CREDENTIALS '{credentials}'"
+
     query_options = {}
-    query_options["bucket"] = backup_bucket
-    query_options["path"] = backup_base_name
     query_options["config"] = json.dumps(backup_config)
     for db in dbs:
-        query_options["db"] = db
-        logging.info(query_template.format(credentials="<redacted>", **query_options))
-        c.execute(query_template.format(credentials=json.dumps(backup_creds), **query_options))
-        c.fetchall()
-        logging.info("Finished backup of database `{}`".format(db))
+        backup_info = FullPathForBackup(c, db, is_incremental, full_backup_only, backup_bucket, backup_prefix)
+        if backup_info.path:
+            query_options["db"] = db
+            if not full_backup_only:
+                query_options["mode"] = "DIFFERENTIAL" if backup_info.is_incremental else "INIT"
+            query_options["path"] = backup_info.path
+            logging.info(query_template.format(credentials="<redacted>", **query_options))
+            c.execute(query_template.format(credentials=json.dumps(backup_creds), **query_options))
+            c.fetchall()
+            if full_backup_only:
+                logging.info("Finished full backup of database `{}` at {}".format(db, backup_info.path))
+            else:
+                logging.info("Finished {} backup of database `{}` at {}".format(query_options["mode"], db, backup_info.path))
 
     logging.info("Done")
