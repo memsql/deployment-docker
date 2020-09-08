97d86b9af4f5dfd343fde0a1cbd38645c81ad8ef
Hari Dhanushkodi <hdhanushkodi@memsql.com>
2020-09-08 13:31:52 -0700

    7.1 rbac changes
    Summary: This diff makes changes to node/dynamic-node + tools image to support the SERVICE_USER engine RBAC change made in 7.1.2. Root will be granted SERVICE_USER permission only if the ENABLE_SERVICE_USER env variable is set. This variable will be set in the helios backend (D41484) and sent by the operator to the image, so SERVICE_USER will only exist for HELIOS clusters. On-prem operator customers should not be affected. Additionally, support accounts will be given the SERVICE_USER permission as well. To make the tools image backwards compatibile, root permissions are checked before granting the support account SERVICE_USER.
    
    Test Plan:
    1. Construct new node/dynamic-node/tools images and update local dev to use these new images.
    2. Create new cluster, login as root and verify SERVICE_USER has been granted.
    3. Create new support account, login with support account and verify SERVICE_USER has been granted.
    4. Repeat test with memsql cluster < 7.1.2, verify nothing breaks.
    
    Reviewers: jkoh, tgummalla
    
    Reviewed By: jkoh, tgummalla
    
    Subscribers: davidgomes, engineering-list
    
    JIRA Issues: MCDB-2314
    
    Differential Revision: https://grizzly.internal.memcompute.com/D41483

--- assets/scripts/util.py
+++ assets/scripts/util.py
@@ -29,6 +29,14 @@ def grant_user_super(conn, username):
     with conn.cursor() as cursor:
         cursor.execute("GRANT ALL ON *.* TO %s", (username,))
 
+def grant_user_service_user(conn, username):
+    with conn.cursor() as cursor:
+        cursor.execute("SHOW GRANTS")
+        grants = cursor.fetchone()[0]
+        # if root has SERVICE_USER, grant SERVICE_USER to support account
+        if "SERVICE_USER" in grants:
+            cursor.execute("GRANT SERVICE_USER ON *.* TO %s", (username,))
+
 def drop_user(conn, username):
     with conn.cursor() as cursor:
         cursor.execute("DROP USER %s", (username,))
