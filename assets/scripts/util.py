6973cc1e3d1f023bada0bb50b41f855c6847469c
Hari Dhanushkodi <hdhanushkodi@memsql.com>
2020-09-28 12:13:12 -0700

    enable SERVICE_USER for support accounts
    Summary: Now that SERVICE_USER is enabled on root for all new helios clusters (D41820), SERVICE_USER should be granted to all new support accounts.
    
    Test Plan: create cluster, create support account, verify support account has SERVICE_USER.
    
    Reviewers: jkoh, tgummalla
    
    Reviewed By: jkoh, tgummalla
    
    Subscribers: engineering-list
    
    JIRA Issues: MCDB-2314
    
    Differential Revision: https://grizzly.internal.memcompute.com/D42099

--- assets/scripts/util.py
+++ assets/scripts/util.py
@@ -25,6 +25,15 @@ def create_user(conn, username, password):
             username, password,
         ))
 
+def grant_user_service_user(conn, username):
+    with conn.cursor() as cursor:
+        cursor.execute("SHOW GRANTS")
+        grants = cursor.fetchone()[0]
+        # if root has SERVICE_USER, grant SERVICE_USER to support account
+        if "SERVICE_USER" in grants:
+            cursor.execute("GRANT SERVICE_USER ON *.* TO %s", (username,))
+            logging.info("GRANT SERVICE_USER ON *.* TO {}".format(username))
+
 def grant_user_super(conn, username):
     with conn.cursor() as cursor:
         cursor.execute("GRANT ALL ON *.* TO %s", (username,))
