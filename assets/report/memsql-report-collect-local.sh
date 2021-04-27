95f8b9950886e1f5618364d742aee57fa4586d65
Daniel Wang <dwang@memsql.com>
2021-04-26 18:55:23 -0700

    Summary: Remove default container logs in cluster report
    Test Plan: Run cluster reports locally, make sure that `kubectl logs <report-pod> | grep 'Defaulting'` doesn't return any results
    
    Reviewers: dalekseyev, noprysk-ua
    
    Reviewed By: noprysk-ua
    
    Subscribers: engineering-list
    
    JIRA Issues: PLAT-5376
    
    Differential Revision: https://grizzly.internal.memcompute.com/D47417

--- assets/report/memsql-report-collect-local.sh
+++ assets/report/memsql-report-collect-local.sh
@@ -11,21 +11,25 @@ REPORT_DIR=$2
 # trying to set an annotation where the key already exists will fail
 kubectl annotate pod ${POD} memsql-report-lock-owner=${S3_REPORT_PATH}
 
+kubectl_exec() {
+  kubectl exec ${POD} -c node -- bash -c "${1}"
+}
+
 function cleanup {
-    kubectl exec ${POD} -- bash -c 'rm -rf /tmp/report' || true
+    kubectl_exec 'rm -rf /tmp/report' || true
     # adding - to the annotation key will make this delete that annotation
     kubectl annotate pod ${POD} memsql-report-lock-owner-
 }
 trap cleanup EXIT
 
-kubectl exec ${POD} -- bash -c 'rm -rf /tmp/report'
-kubectl exec ${POD} -- bash -c 'mkdir /tmp/report'
+kubectl_exec 'rm -rf /tmp/report'
+kubectl_exec 'mkdir /tmp/report'
 
-kubectl cp $(which sdb-report) ${POD}:tmp/report/sdb-report
+kubectl cp -c node $(which sdb-report) ${POD}:tmp/report/sdb-report
 # support running reports for root and non-root users
-kubectl exec ${POD} -- bash -c 'export USER=$(whoami) && echo user=\"${USER}\" > /tmp/report/tb_config';
+kubectl_exec 'export USER=$(whoami) && echo user=\"${USER}\" > /tmp/report/tb_config';
 # we set XDG_DATA_HOME because reports tries to create a directory there and
 # it defaults to $HOME/.local/... which non-root users do not have write permission
-kubectl exec ${POD} -- bash -c 'export XDG_DATA_HOME=/tmp/report && /tmp/report/sdb-report collect-local '"${COLLECTOR_FLAGS}"' -c /tmp/report/tb_config -o /tmp/report/report.tar.gz --opt memsqlTracelogs.tracelogSize=100mb --hostname '${POD}''
+kubectl_exec 'export XDG_DATA_HOME=/tmp/report && /tmp/report/sdb-report collect-local '"${COLLECTOR_FLAGS}"' -c /tmp/report/tb_config -o /tmp/report/report.tar.gz --opt memsqlTracelogs.tracelogSize=100mb --hostname '${POD}''
 # remove leading slash from temp_dir for kubectl cp
-kubectl cp ${POD}:tmp/report/report.tar.gz ${REPORT_DIR}/${POD}
+kubectl cp -c node ${POD}:tmp/report/report.tar.gz ${REPORT_DIR}/${POD}
