6e130a03582ca8ad0cc59b559a6b70fe0b054f74
Daniel Wang <dwang@memsql.com>
2021-09-08 15:55:46 -0700

    remove parallel

--- assets/report/cluster-report-lib.sh
+++ assets/report/cluster-report-lib.sh
@@ -2,15 +2,6 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then echo "MUST BE SOURCED!"; exit 1; fi
 
 # Define functions to control how reports are run on the cluster
 
-# function getParallelism() {
-#     # Max parallelism of 4x CPU, or reduce if fewer pods.
-#     ((_PARALLELISM=$(nproc)*4))
-#     TMP=($(cut -d"," --output-delimiter=" " -f1- <<< "$(getPods)"))
-#     POD_COUNT=${#TMP[@]}
-#     if [[ POD_COUNT -lt _PARALLELISM ]]; then _PARALLELISM=${POD_COUNT}; fi
-#     echo "${_PARALLELISM}"
-# }
-
 function getCluster_collection_script() { echo "./memsql-report-collect.sh"; }
 
 function getAWS_command() {
