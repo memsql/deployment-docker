2b59964525b5ab6578d1837b3e10f2215dbb6fd8
Szu-Po Wang <szupo@memsql.com>
2022-06-13 19:28:14 -0700

    Run ADD LEAF on engine directly in ci test
    Summary:
    Work around PLAT-6235, where the ctl add leaf command was failing due
    to the node already has the role LEAF. Since the role check is ctl-only,
    adding leaf from the engine directly avoids the issue.
    
    Test Plan:
    test passed on the ci
    https://app.circleci.com/pipelines/github/memsql/deployment-docker/360/workflows/e0b14a79-7896-4652-b225-4ebcc874b9ca
    (while it failed for me 5 times consecutively on master)
    
    Reviewers: adam, rodrigo, davidgomes, vtkachuk-ua
    
    Reviewed By: rodrigo
    
    Subscribers: engineering-list
    
    Differential Revision: https://grizzly.internal.memcompute.com/D57171

--- test/lib.sh
+++ test/lib.sh
@@ -149,7 +149,11 @@ add-leaf() {
     local master=${1}
     local leaf=${2}
     echo "adding leaf ${leaf} to ${master}"
-    memsqlctl ${master} add-leaf --host $(docker-ip ${leaf}) --password ${ROOT_PASSWORD}
+
+    # Running ADD LEAF query against the engine directly instead of using the ctl add leaf
+    # command to work around PLAT-6235 (ctl add leaf failing due to the node already
+    # having role LEAF).
+    memsqlctl ${master} query -e "ADD LEAF 'root':'${ROOT_PASSWORD}'@'$(docker-ip ${leaf})':3306"
 }
 
 remove-leaf() {
