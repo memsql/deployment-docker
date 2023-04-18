6d0463a425511601a80069f2a0cdb1d5a4ca38ed
Brooks Remy <bremy@singlestore.com>
2023-04-17 18:19:12 -0700

    Revert "adding validation that there are no other memsqld_safe running before entrypoint"
    This reverts commit a7d436900676570a1c7e41af3fe419bfb66bb194.

--- assets/lib.sh
+++ assets/lib.sh
@@ -99,13 +99,6 @@ startSingleStore() {
     local memsqldPath=${installDir}/memsqld
     local memsqldSafePath=${installDir}/memsqld_safe
 
-    # as a sanity check, make sure there isn't an existing process running memsqldSafePath
-    existing_memsql_pid=$(pgrep -f ${memsqldSafePath})
-    if [[ "$existing_memsql_pid" != "" ]]; then
-        log "Error, there is an existing memsql process running '$memsqldSafePath' with PID $existing_memsql_pid"
-        exit 1
-    fi
-
     env -u ROOT_PASSWORD ${memsqldSafePath} \
         --auto-restart disable \
         --defaults-file $(memsqlctl describe-node --property MemsqlConfig) \
